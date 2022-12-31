81055c2dff1b4b1bd64e8cffb49ee9d9208b5f9b
iaace-NA <23423640+iaace-NA@users.noreply.github.com>
2022-12-31 14:43:19 -0800

    Allow no-timeline

--- docs/index.js
+++ docs/index.js
@@ -113,30 +113,30 @@ let spell_data;
 let champion_data;
 let rune_data;
 loadJSON(match_url).then(match_data => {
-            let match = new Match(champion_data, match_data, null, true);
-            const major_patch = match.gameVersion.substring(0, match.gameVersion.indexOf(".", match.gameVersion.indexOf(".") + 1));
-            addv = major_patch + ".1"; //active ddragon version
-            $("metadata").innerHTML = `<h1>${queues[match.queueId]}</h1>
-    <p class="m-1">${new Date(match.gameCreation).toLocaleDateString()} ${new Date(match.gameCreation).toLocaleTimeString()}</p>
-    <p class="m-1">Region: ${escapeHtml(regions[match.platformId])}, Match ID: ${escapeHtml(match.gameId)}, Patch ${escapeHtml(major_patch)}, Duration: ${standardTimestamp(match.gameDuration)}</p>`;
-            Promise.all([
-                    loadJSON(`https://ddragon.leagueoflegends.com/cdn/${addv}/data/en_US/champion.json`),
-                    loadJSON(match_timeline_url),
-                    loadJSON(`https://ddragon.leagueoflegends.com/cdn/${addv}/data/en_US/summoner.json`),
-                    loadJSON(`https://ddragon.leagueoflegends.com/cdn/${addv}/data/en_US/runesReforged.json`)
-                ]).then(responses => {
-                        console.log(responses);
-                        champion_data = responses[0];
-                        const timeline_data = responses[1];
-                        spell_data = responses[2];
-                        rune_data = responses[3];
-                        match = new Match(champion_data, match_data, timeline_data, true);
-                        console.log(match);
-                        let teams = match.teams.map((team, team_index) => {
-                                    //____, ______, ______, Level, _____, Team #       , _____, _____, _____, _____, _____, _____, _____, _________, __, ____
-                                    //Rune, Spell1, Spell2, Level, Champ, Summoner Name, Item0, Item1, Item2, Item3, Item4, Item5, Item6, K / D / A, CS, Gold
-                                    console.log(team.bans);
-                                    return `<thead class="sticky"><tr>
+    let match = new Match(champion_data, match_data, null, true);
+    const major_patch = match.gameVersion.substring(0, match.gameVersion.indexOf(".", match.gameVersion.indexOf(".") + 1));
+    addv = major_patch + ".1"; //active ddragon version
+    $("metadata").innerHTML = `<h1>${queues[match.queueId]}</h1>
+        <p class="m-1">${new Date(match.gameCreation).toLocaleDateString()} ${new Date(match.gameCreation).toLocaleTimeString()}</p>
+        <p class="m-1">Region: ${escapeHtml(regions[match.platformId])}, Match ID: ${escapeHtml(match.gameId)}, Patch ${escapeHtml(major_patch)}, Duration: ${standardTimestamp(match.gameDuration)}</p>`;
+    Promise.all([
+        loadJSON(`https://ddragon.leagueoflegends.com/cdn/${addv}/data/en_US/champion.json`),
+        loadJSON(match_timeline_url, true),
+        loadJSON(`https://ddragon.leagueoflegends.com/cdn/${addv}/data/en_US/summoner.json`),
+        loadJSON(`https://ddragon.leagueoflegends.com/cdn/${addv}/data/en_US/runesReforged.json`)
+    ]).then(responses => {
+            console.log(responses);
+            champion_data = responses[0];
+            const timeline_data = responses[1];
+            spell_data = responses[2];
+            rune_data = responses[3];
+            match = new Match(champion_data, match_data, timeline_data, true);
+            console.log(match);
+            let teams = match.teams.map((team, team_index) => {
+                //____, ______, ______, Level, _____, Team #       , _____, _____, _____, _____, _____, _____, _____, _________, __, ____
+                //Rune, Spell1, Spell2, Level, Champ, Summoner Name, Item0, Item1, Item2, Item3, Item4, Item5, Item6, K / D / A, CS, Gold
+                console.log(team.bans);
+                return `<thead class="sticky"><tr>
             ${headerText("Rune")}
             ${headerText("Spells")}
             ${headerText("Level")}
@@ -147,25 +147,25 @@ loadJSON(match_url).then(match_data => {
             ${headerText("CS")}
             ${headerText("Gold")}
             </tr></thead>${match.participants.map(p => {
-                if (p.teamId != team.teamId) return "";
-                let pI = match.participantIdentities.find(pI => p.participantId == pI.participantId);
-                return `<tr class="match-${p.stats.win ? "victory" : "defeat"}">
-                <td>${runeIDtoImg(p.stats.perk0)}</td>
-                <td>${spellIDtoImg(p.spell1Id)}${spellIDtoImg(p.spell2Id)}</td>
-                ${cellText(p.stats.champLevel)}
-                <td>${championIDtoImg(p.championId)}</td>
-                ${cellText(pI.player.summonerName)}
-                <td class="tal">${itemIDtoImg(p.stats.item0)}
-                ${itemIDtoImg(p.stats.item1)}
-                ${itemIDtoImg(p.stats.item2)}
-                ${itemIDtoImg(p.stats.item3)}
-                ${itemIDtoImg(p.stats.item4)}
-                ${itemIDtoImg(p.stats.item5)}
-                ${itemIDtoImg(p.stats.item6, "item-img ms-5")}</td>
-                ${cellText(`${p.stats.kills} / ${p.stats.deaths} / ${p.stats.assists}`)}
-                ${cellText(p.stats.neutralMinionsKilled + p.stats.totalMinionsKilled)}
-                ${cellText(p.stats.goldEarned)}</tr>`;
-            }).join("")}`;
+            if (p.teamId != team.teamId) return "";
+            let pI = match.participantIdentities.find(pI => p.participantId == pI.participantId);
+            return `<tr class="match-${p.stats.win ? "victory" : "defeat"}">
+            <td>${runeIDtoImg(p.stats.perk0)}</td>
+            <td>${spellIDtoImg(p.spell1Id)}${spellIDtoImg(p.spell2Id)}</td>
+            ${cellText(p.stats.champLevel)}
+            <td>${championIDtoImg(p.championId)}</td>
+            ${cellText(pI.player.summonerName)}
+            <td class="tal">${itemIDtoImg(p.stats.item0)}
+            ${itemIDtoImg(p.stats.item1)}
+            ${itemIDtoImg(p.stats.item2)}
+            ${itemIDtoImg(p.stats.item3)}
+            ${itemIDtoImg(p.stats.item4)}
+            ${itemIDtoImg(p.stats.item5)}
+            ${itemIDtoImg(p.stats.item6, "item-img ms-5")}</td>
+            ${cellText(`${p.stats.kills} / ${p.stats.deaths} / ${p.stats.assists}`)}
+            ${cellText(p.stats.neutralMinionsKilled + p.stats.totalMinionsKilled)}
+            ${cellText(p.stats.goldEarned)}</tr>`;
+        }).join("")}`;
         }).join("<tr><td>&nbsp;</td></tr>");
         teams = "<table class=\"table\">" + teams + "</table>";
         $("scoreboard").innerHTML = teams;
@@ -237,16 +237,23 @@ function getParameterByName(name, url) {
     return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
 
-function loadJSON(url) {
-    return new Promise((resolve, reject) => {
-        let request = new Request(url, { method: "GET" });
-        fetch(request).then(response => {
-            resolve(response.json());
-        }).catch(error => {
-            console.error(error);
-            reject(error);
+function loadJSON(url, allow_null = false) {
+    if (url === undefined || url === null && allow_null) {
+        return new Promise((resolve, reject) => {
+            resolve(null);
         });
-    });
+    }
+    else {
+        return new Promise((resolve, reject) => {
+            let request = new Request(url, { method: "GET" });
+            fetch(request).then(response => {
+                resolve(response.json());
+            }).catch(error => {
+                console.error(error);
+                reject(error);
+            });
+        });
+    }
 }
 
 function handleError(err) {
